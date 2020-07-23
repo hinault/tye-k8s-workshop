83085ff75bf8dff954086ae79ef071e93d5f97d9
Hinault <ngdonfack@gmail.com>
2020-07-23 15:14:40 -0400

    Add tye support

--- MvcAppClient/Startup.cs
+++ MvcAppClient/Startup.cs
@@ -39,7 +39,7 @@ namespace MvcAppClient
                 .AddOpenIdConnect("oidc", options =>
                 {
 
-                    options.Authority = "https://localhost:5001";
+                    options.Authority = Configuration.GetServiceUri("AspNetCoreIdentityServer").AbsoluteUri; // "https://localhost:5001";
                     options.RequireHttpsMetadata = false;
                     options.ClientSecret = "secret";
                     options.ResponseType = "code";
