97c0bbc0b887d90e705d1ca28a585864ea1f9425
Hinault <ngdonfack@gmail.com>
2020-10-10 07:02:24 -0400

    Ajout config

--- TestApi/Startup.cs
+++ TestApi/Startup.cs
@@ -30,7 +30,7 @@ namespace TestApi
             services.AddAuthentication("Bearer")
              .AddJwtBearer("Bearer", options =>
              {
-                 options.Authority = Configuration.GetServiceUri("AspNetCoreIdentityServer").AbsoluteUri;
+                 options.Authority = Configuration.GetValue<string>("AspNetCoreIdentityServer");
                  options.RequireHttpsMetadata = false;
 
                  options.Audience = "testapi";
