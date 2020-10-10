7d2b8af3fefa32c2dcb08d86dd634db2ce08302e
Hinault <ngdonfack@gmail.com>
2020-10-10 07:57:35 -0400

    Update WeatherClient.cs

--- MvcAppClient/WeatherClient.cs
+++ MvcAppClient/WeatherClient.cs
@@ -1,11 +1,33 @@
-﻿using System;
+﻿using MvcAppClient.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace MvcAppClient
 {
     public class WeatherClient
     {
+        private readonly JsonSerializerOptions options = new JsonSerializerOptions()
+        {
+            PropertyNameCaseInsensitive = true,
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        };
+
+        private readonly HttpClient client;
+
+        public WeatherClient(HttpClient client)
+        {
+            this.client = client;
+        }
+
+        public async Task<WeatherForecast[]> GetWeatherAsync()
+        {
+            var responseMessage = await this.client.GetAsync("/weatherforecast");
+            var stream = await responseMessage.Content.ReadAsStreamAsync();
+            return await JsonSerializer.DeserializeAsync<WeatherForecast[]>(stream, options);
+        }
     }
 }
